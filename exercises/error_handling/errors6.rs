33249e9094571e53f1f353f1b517381c751f3a34
Richard Melkonian <r.v.melkonian@gmail.com>
2022-07-22 21:40:21 +0100

    completed generics

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,8 +8,6 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -20,17 +18,26 @@ enum ParsePosNonzeroError {
 }
 
 impl ParsePosNonzeroError {
-    // TODO: add another error conversion function here.
+ fn from_creation(error: CreationError) -> ParsePosNonzeroError {
+     ParsePosNonzeroError::Creation(error)
+ }
+ fn from_parse_int(error: ParseIntError) -> ParsePosNonzeroError {
+     ParsePosNonzeroError::ParseInt(error)
+ }
 }
 
 fn parse_pos_nonzero(s: &str)
     -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
 {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    match s.parse() {
+        Ok(n) => {
+            match PositiveNonzeroInteger::new(n) {
+                Ok(v) => Ok(v),
+                Err(e) => Err(ParsePosNonzeroError::from_creation(e))
+            }
+        }
+        Err(e) => Err(ParsePosNonzeroError::from_parse_int(e))
+    }
 }
 
 // Don't change anything below this line.
--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,12 +2,11 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
 }
